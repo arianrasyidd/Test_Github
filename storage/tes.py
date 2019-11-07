a92662fb70d231ffc3f22641d4623b82e5be0596
arianrasyidd <arianrasyids@gmail.com>
2019-11-07 10:36:43 +0700

    penjelasan tiap log dari git

--- storage/tes.py
+++ storage/tes.py
@@ -1,1 +1,14 @@
 print('tes')
+
+# git add . #mendaftarkan semua file ke stage
+# git add (nama file) #mendaftarkan file tertentu ke stage
+# git init 
+# git status
+# git commit -m
+# git log
+# git log oneline
+# git checkout (id-nya) #ngeliat2 checkpointnya
+# git checkout master #balikin ke yang paling baru
+# git rm --cached (nama file) #Ngerubah status dari A ke U
+# git reset (id file) #buat balik ke commit awal tapi file setelahnya masih ada, hanya dianggap baru
+# git reset (id file) -- hard #buat ngehapus commit setelah itu
