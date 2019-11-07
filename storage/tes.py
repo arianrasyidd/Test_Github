e59ada6eaaa6c58324a39b6cc84a6f2752428239
arianrasyidd <arianrasyids@gmail.com>
2019-11-07 10:49:09 +0700

    percobaan branch

--- storage/tes.py
+++ storage/tes.py
@@ -12,3 +12,7 @@ print('tes')
 # git rm --cached (nama file) #Ngerubah status dari A ke U
 # git reset (id file) #buat balik ke commit awal tapi file setelahnya masih ada, hanya dianggap baru
 # git reset (id file) -- hard #buat ngehapus commit setelah itu
+# git branch (nama fitur / nama orang yang ngerjainnya)
+# git branch -a #buat ngeliat apa aja cabang yang kita punya #tanda bintang berarti kita ada disitu
+# git branch -D (nama cabangnya) #buat ngehapus cabang
+# git checkout (nama branchnya)
